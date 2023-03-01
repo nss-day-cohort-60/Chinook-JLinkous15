e201c02697a89417a6a92675586ee358430a9f64
James Linkous <james.william.linkous@gmail.com>
2023-03-01 12:19:18 -0600

    small changes to return only Brazilian customers

--- brazil_customers_invoices.sql
+++ brazil_customers_invoices.sql
@@ -1,5 +1,9 @@
 SELECT
-*
+C.FirstName || ' ' || C.LastName AS FullName,
+I.InvoiceId,
+I.InvoiceDate,
+I.BillingCountry
 FROM Customer AS C
 JOIN Invoice AS I 
     ON C.CustomerId = I.CustomerId
+WHERE C.Country LIKE "brazil";
