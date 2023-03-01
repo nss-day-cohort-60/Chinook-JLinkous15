5346afeee6e3478ad39249765413a07ecc835d7e
James Linkous <james.william.linkous@gmail.com>
2023-03-01 15:43:59 -0600

    code that actually completes the ticket.

--- line_item_track_artist.sql
+++ line_item_track_artist.sql
@@ -1,9 +1,11 @@
 SELECT
 i.InvoiceId,
 t.Name AS TrackName,
-a.title AS AlbumName
+Artist.Name
 FROM InvoiceLine AS i
 JOIN Track AS t
     ON t.TrackId = i.TrackId
 JOIN Album AS a
     ON t.AlbumId = a.AlbumId
+JOIN Artist
+    ON Artist.ArtistId = a.ArtistId
